6135af82862a15eff1493cc8367c62af29d7005e
marrlupus <99767913+marrlupus@users.noreply.github.com>
2022-03-01 14:03:31 -0300

    Update ex_04_01.py

--- ex_04_01.py
+++ ex_04_01.py
@@ -1,4 +1,3 @@
-#La definición de la función es algebraica.
 def computepay(a, b):
     if a>40 :
         return(40*b+((a-40)*(b*1.5)))
@@ -10,5 +9,4 @@ tarf= input('tarifa por hora: ')
 h=float(hrs)
 t=float(tarf)
 p = computepay(h, t)
-#aca p me devuelve el RESULTADO de alguna de las dos ecuaciones según los valores dados
 print("Pay", p)
